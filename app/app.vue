e766cf8ba3686b90c0e9051252afdc8d41db7fdf
otmane <othmandak87@gmail.com>
2026-03-15 14:42:04 +0000

    setup taillwind / daisy UI / icons

--- app/app.vue
+++ app/app.vue
@@ -1,44 +1,8 @@
 <template>
-  <div class="travel-container">
-    <h1>Welcome to Travel Log</h1>
-    <p>Loading your adventures...</p>
-    <p><i>(Or just loading my desire to be on vacation)</i></p>
-    <div class="plane-animation">
-      ✈️
-    </div>
-    <div class="scenery">
-      🐢 🌴🌴🌴
-    </div>
-    <p>Actual travel status: Walking from the bed to the fridge.</p>
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-secondary">
+    View Map <Icon name="material-symbols:map-rounded" size="30" />
+  </button>
 </template>
-
-<style scoped>
-.travel-container {
-  text-align: center;
-  margin-top: 50px;
-  font-family: sans-serif;
-}
-
-.plane-animation {
-  font-size: 50px;
-  display: inline-block;
-  animation: fly 3s linear infinite;
-}
-
-.scenery {
-  font-size: 50px;
-}
-
-@keyframes fly {
-  0% {
-    transform: translateX(-150px) translateY(0);
-  }
-  50% {
-    transform: translateX(0) translateY(-20px);
-  }
-  100% {
-    transform: translateX(150px) translateY(0);
-  }
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  ssr: false,
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
