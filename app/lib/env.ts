4b255a0ab8bbf4222306242beedf12bed785960c
otmane <othmandak87@gmail.com>
2026-03-17 10:33:39 +0000

    setup authentification feature with better auth

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -5,6 +5,8 @@ export const EnvSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   TURSO_DATABASE_URL: z.string().optional(),
   TURSO_AUTH_TOKEN: z.string().optional(),
+  BETTER_AUTH_SECRET: z.string().optional(),
+  BETTER_AUTH_URL: z.string().optional(),
 
   // Add environment variables here (e.g., API_BASE_URL, etc.)
 });
